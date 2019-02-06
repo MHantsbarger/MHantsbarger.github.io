4de186a440514f2e0c7d0d292aea45ff9c9a35ae
Matthew Hantsbarger <m.hantsbarger@comcast.net>
2019-02-06 18:48:31 -0500

    completed d3 drawing squares function homework

---
+++ homework/d3hw.js
@@ -0,0 +1,26 @@
+var svg = d3.select("svg");
+var squares = svg.selectAll(".square");
+var oneSqColors = ["black"];
+var threeSqColors=["cyan","magenta","yellow"];
+var sixSqColors=["red","orange","yellow","green","blue","purple"];
+
+function drawSquares(width,colors) {
+    
+    var colorData = squares.data(colors);
+    colorData.enter().append("rect")
+        .attr("class", "square")
+        .attr("x", function(d,i) {return i*width+5;})
+        .attr("y", 5)
+        .attr("width", width)
+        .attr("height", width)
+        .attr("fill", function(d) {return d;});
+    colorData.exit().remove();
+
+    colorData
+        .attr("x", function(d,i) {return i*width+5;})
+        //.attr("y", 5)
+        .attr("width", width)
+        .attr("height", width)
+        .attr("fill", function(d) {return d;});
+    squares=svg.selectAll(".square");
+}
