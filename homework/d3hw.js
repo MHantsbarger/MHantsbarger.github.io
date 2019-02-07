b1a9650c677e4634fa1e6fffa0063edaf952544c
Matthew Hantsbarger <m.hantsbarger@comcast.net>
2019-02-07 18:55:21 -0500

    corrected homework from feedback (AFTER DUE)

--- homework/d3hw.js
+++ homework/d3hw.js
@@ -1,12 +1,14 @@
 var svg = d3.select("svg");
-var squares = svg.selectAll(".square");
+
 var oneSqColors = ["black"];
 var threeSqColors=["cyan","magenta","yellow"];
 var sixSqColors=["red","orange","yellow","green","blue","purple"];
 
 function drawSquares(width,colors) {
-    
+
+    squares=svg.selectAll(".square");
     var colorData = squares.data(colors);
+
     colorData.enter().append("rect")
         .attr("class", "square")
         .attr("x", function(d,i) {return i*width+5;})
@@ -14,13 +16,16 @@ function drawSquares(width,colors) {
         .attr("width", width)
         .attr("height", width)
         .attr("fill", function(d) {return d;});
-    colorData.exit().remove();
-
+        
     colorData
         .attr("x", function(d,i) {return i*width+5;})
         //.attr("y", 5)
         .attr("width", width)
         .attr("height", width)
         .attr("fill", function(d) {return d;});
-    squares=svg.selectAll(".square");
+
+    colorData.exit().remove();
+
+    
+    
 }
