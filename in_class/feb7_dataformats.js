574c42443618ccff5a13b1e15bcf14d01b69a89b
Matthew Hantsbarger <m.hantsbarger@comcast.net>
2019-02-14 18:05:56 -0500

    added data exploration

--- in_class/feb7_dataformats.js
+++ in_class/feb7_dataformats.js
@@ -6,4 +6,18 @@ d3.text("/data/TextData.txt", function(error, data) {
 d3.csv("/data/CSVdata.csv", function(error, data) {
     console.log("csv data error:", error);
     console.log("csv contents:", data);
-  });
+    data.forEach ( 
+        function(d){ 
+            d.export = parseFloat(d.export);
+            d.country = parseFloat(d.country);
+            console.log(d);
+        }
+    );
+});
+
+d3.json("/data/JSONdata.json", function(error, data) {
+    console.log("JSON data error:", error);
+    console.log("JSON contents:", data);
+});
+
+console.log("This stuff doesn't output in order.");
