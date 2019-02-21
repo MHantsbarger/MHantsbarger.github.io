4ab894dc525d47b19d95caa0a16b0310255ca8e3
Matthew Hantsbarger <m.hantsbarger@comcast.net>
2019-02-21 18:49:43 -0500

    modified homework javascript and homepage to process csv data correctly.

--- homework/finaldata.js
+++ homework/finaldata.js
@@ -5,18 +5,18 @@ d3.csv("/data/Final_Project_Data/vgsales.csv", function(error, data) {
     console.log("csv contents:", data);
     data.forEach ( 
         function(d){ 
-            d.rank = parseFloat(d.rank); // Sales rank
-            d.name = parseFloat(d.name); // Name of game
-            d.platform = parseFloat(d.platform); // Release platform for the game
-            d.year = parseFloat(d.year); // Year of release (Since 1980 to 2016)
-            d.genre = parseFloat(d.genre); // Game genre
-            d.publisher = parseFloat(d.publisher); // Company which released the game
-            d.salesNA = parseFloat(d.salesNA); // North american sales (in millions of copies)
-            d.salesEU = parseFloat(d.salesEU); // European sales
-            d.salesJPN = parseFloat(d.salesJPN); // Japan sales
-            d.salesOther = parseFloat(d.salesOther); // Sales in other countries
-            d.salesTotal = parseFloat(d.salesTotal); // Global_Sales
-            console.log(d);
+            d.rank = parseInt(d.Rank); // Sales rank
+            d.name = d.Name; // Name of game
+            d.platform = d.Platform; // Release platform for the game
+            d.year = parseInt(d.Year); // Year of release (Since 1980 to 2016)
+            d.genre = d.Genre; // Game genre
+            d.publisher = d.Publisher; // Company which released the game
+            d.salesNA = parseFloat(d.EU_Sales) * 1000000; // European sales
+            d.salesJPN = parseFloat(d.JP_Sales) * 1000000; // Japan sales
+            d.salesOther = parseFloat(d.Other_Sales) * 1000000; // Sales in other countries
+            d.salesTotal = parseFloat(d.Global_Sales) * 1000000; // Global Sales
+            // console.log(d);
         }
     );
+    console.log(data);
 });
