3e2a51f43973fedaa9a68be258b79e7bc044645e
Matthew Hantsbarger <m.hantsbarger@comcast.net>
2019-03-14 17:51:00 -0400

    first visualization is top 10, scale is off

--- final_project/video_game_data.js
+++ final_project/video_game_data.js
@@ -19,9 +19,70 @@ d3.csv("/data/Final_Project_Data/vgsales.csv", function(error, data) { //load an
             // console.log(d);
         }
     );
+    
+    var xScale = d3.scaleLinear()
+    .domain([1,11])
+    .range([350, .97*window.innerWidth]);
 
+    var yScale = d3.scaleLinear()
+        .domain([0, 90000000])
+        .range([120, 460]);
+
+
+    var svg = d3.select("svg");
+    // var bgRect = svg.selectAll("#bgRect")
+    // x="5px" y="100px" width="98%" height="500px" fill="white" stroke-width="1" stroke="black"
+    var topTen = data.filter(function(d){
+        return d.rank <= 10;
+    });
+    console.log(topTen);
+
+    var barGrBars = svg.selectAll("barRect")
+        .data(topTen);
+
+    barGrBars.enter().append("rect")
+        .attr("id", "barRect")
+        .attr("x", function(d) {
+            return xScale(d.rank);
+        })
+        .attr("y", function(d) {
+            // return yScale(d.salesTotal);
+            return 120;
+        })
+        .attr("width", function(d) {
+            return ((window.innerWidth-270)/10)-35;
+            // return 20;
+        })
+        .attr("height", function(d) {
+            return (yScale(d.salesTotal));
+        })
+        .attr("fill", function(d) {
+            if (d.rank%2 == 0) {
+                return "black";
+            }
+            else {
+                return "grey";
+            }
+        });
+    
+    // d3.selectAll("barRect")
+    //     .on("click", function(d) {
+    //         console.log(d.name);
+    //     })
+    //     .on("mousemove", function(d) {
+    //         var mouse = d3.mouse(this);
+    //         d3.select("#tooltip")
+    //             .style("display", "block")
+    //             .html("<h1>" + d.name + "</h1>")
+    //             .style("left", mouse[0] + "px")
+    //             .style("top", mouse[1] - 50 + "px");
+    //     })
+    //     .on("mouseout", function(d) {
+    //         d3.select("#tooltip")
+    //             .style("display", "none")
+    //     });
+    var axis = d3.axisLeft(yScale);
+    d3.select("#yAxis").call(axis);
 });
 
-var svg = d3.select("svg");
-var bgRect = svg.selectAll("#bgRect")
-// x="5px" y="100px" width="98%" height="500px" fill="white" stroke-width="1" stroke="black"
+
